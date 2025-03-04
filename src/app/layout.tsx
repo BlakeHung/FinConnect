8eea0b6ddb8ad1b12fc25f7462a9b83af8f01436
Blake <mars7823482@gmail.com>
2025-03-04 15:10:08 +0800

    chore: edit layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,7 +78,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
       </head>
-      <body className="min-h-screen flex flex-col">
+      <body className="min-h-screen">
         {children}
       </body>
     </html>
